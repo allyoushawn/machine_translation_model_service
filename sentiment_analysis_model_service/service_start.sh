b5342d50dfa5bd7d1c1e031731207344f9356d84
Yu-Hsuan Wang <k43272001@gmail.com>
2023-02-16 21:36:59 -0800

    gix bash syntax

--- sentiment_analysis_model_service/service_start.sh
+++ sentiment_analysis_model_service/service_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "BUILD_ENV is $BUILD_ENV"
-if [ "$BUILD_ENV" == "jenkins" ]; then
+if [ "$BUILD_ENV" = "jenkins" ]; then
     echo "Downloading credentials"
 else
     echo "BUILD_ENV not jenkins, no need to download credientials"
@@ -11,7 +11,7 @@ fi
 
 gunicorn_port=4460
 
-if [ "$BUILD_ENV" == "jenkins" ]; then
+if [ "$BUILD_ENV" = "jenkins" ]; then
     echo "Start service with credentials"
     gunicorn -w 3 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:$gunicorn_port sentiment_analysis_model_service.entrypoint --certfile=/path/to/file.crt --keyfile=/path/to/file.key
 else
